4dc864d78deba2ef7920b9f27a8f9ea92ed52def
Robin Zerr <zerrrobin@gmail.com>
2026-03-22 19:00:01 -0700

    Refactor top_level_CPU skeleton, update pipeline_registers and decode

--- decode.vhd
+++ decode.vhd
@@ -22,3 +22,5 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+
+
--- pipeline_registers.vhd
+++ pipeline_registers.vhd
@@ -34,16 +34,26 @@ package pipeline_registers is
         dest_reg   : std_logic_vector(2  downto 0); -- destination register index (ra)
         pc_plus2   : std_logic_vector(15 downto 0); -- PC+2 (for BRL return address)
 
-        -- ── Memory control signals (consumed in MEM) ───────
+        -- ── Memory control signals (consumed in MEM - used for write to RAM) ───────
         wr_en_MEM  : std_logic;
 
         -- ── Write-back control signals (riding along to WB) ─
         reg_write  : std_logic;                     -- '1' = write to reg file
         wb_src     : std_logic;                     -- '0'=ALU result, '1'=mem data
 
-        -- ── Memory control outputs (to data RAM) ───────────
-        wr_en_MEM : std_logic
     end record;
+    
+    type MEM_WB is record 
+    
+        alu_result : std_logic_vector(15 downto 0);
+        rd_data2   : std_logic_vector(15 downto 0;
+        dest_reg   : std_logic_vector(15 downto 0);
+        pc_plus2   : std_logic_vector(15 downto 0);
+        
+        reg_write  : std_logic; --Consumed at this stage; 
+        wb_src     : std_logic; --Consumed at this stage;
+        
+    end record; 
 
 
 end package pipeline_registers;
