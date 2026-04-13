efb332ce27fa8a1cc0e1ac50b4a6676f3af2f3d0
Ava <evebirt@gmail.com>
2026-04-12 18:52:47 -0700

    the format B test part 2 completed with expected values

--- src/top_level_cpu.vhd
+++ src/top_level_cpu.vhd
@@ -201,11 +201,11 @@ begin
                 IF_ID_reg.pc_plus2    <= (others => '0');
             elsif if_id_en = '1' then -- operate as normal
                 IF_ID_reg.instruction <= fetch_instruction;
-                IF_ID_reg.pc_plus2    <= std_logic_vector(unsigned(fetch_pc));
-            end if;            
+                IF_ID_reg.pc_plus2    <= std_logic_vector(unsigned(fetch_pc));            
             else -- hold the value in our register, this means there is a stall
                 IF_ID_reg.instruction <= IF_ID_reg.instruction;
                 IF_ID_reg.pc_plus2    <= IF_ID_reg.pc_plus2;
+             end if;         
         end if;
     end process;
 
